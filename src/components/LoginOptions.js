b54bd7cf4f57150057f026070d25ae57d1d86499
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-14 12:46:06 -0500

    Add a LoginOptions to show user when the first navigate to app giving options to sign up/sign in

---
+++ src/components/LoginOptions.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom'
+import dollarLogo from '../images/dollar-logo.png';
+// import SignUp from '../components/forms/SignUp';
+
+const LoginOptions = () => (
+  <div className='container text-center p-5'>
+
+    <div className='text-center pb-5'>
+      <img src={dollarLogo} style={{width: '10rem'}} alt='Dollar Sign Logo' />
+    </div>
+
+    <Link to='/sign-up' className='btn btn-primary mr-5'>Sign Up</Link>
+
+    <Link to='/login' className='btn btn-primary mr-5'>Login</Link>
+
+    <Link to='/dashboard' className='btn btn-primary'>Dashboard</Link>
+  </div>
+);
+
+export default LoginOptions;
