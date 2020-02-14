410a598e806db8d747fe9b15823c36603d82ba2e
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-14 12:42:01 -0500

    Add a AppRoutesContainer to hold all of the app routing statments

---
+++ src/containers/AppRoutesContainer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import NotFound from '../components/NotFound';
+import LoginOptions from '../components/LoginOptions';
+import SignUp from '../components/SignUp';
+
+const AppRoutesContainer = () => (
+  <>
+  <Switch>
+    {/* <Route exact path='/' component={App} /> */}
+    {/* <Route exact path='/sign-up' component={SignUp} />
+    <Route exact path='/login' component={Login} />
+    <Route exact path='/dashboard' component={Dashboard} />
+    <Route exact path='/expenses' component={ExpensesContainer} />
+    <Route exact path='/bills' component={BillsContainer} />
+    <Route exact path='/earnings' component={EarningsContainer} /> */}
+    <Route exact path='/sign-up' component={SignUp} />
+    <Route exact path='/' component={LoginOptions} />
+    <Route component={NotFound} />
+  </Switch>
+  </>
+);
+
+export default AppRoutesContainer;
