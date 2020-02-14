b9b3ee6bcf7992f414f13c3312485e63ef100b02
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-14 11:31:34 -0500

    Add new AppLayout to the index.js so everything will render within this component

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './CSS/index.css';
 import App from './App';
+import AppLayout from './layouts/AppLayout';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <AppLayout>
+    <App />
+  </AppLayout>
+  , document.getElementById('root'));
