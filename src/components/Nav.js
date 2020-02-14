54abba2403db88edf4e33b85049f8cc4b873b664
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-14 11:54:18 -0500

    Add Nav component with our app links in it

---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import React, { PureComponent } from 'react';
+import { NavLink } from 'react-router-dom';
+
+class Nav extends PureComponent {
+
+  render() {
+    return (
+      <nav>
+        <NavLink to='/expenses' className='btn btn-info mr-5'>Expenses</NavLink>
+        <NavLink to='/bills' className='btn btn-info mr-5'>Bills</NavLink>
+        <NavLink to='/earnings' className='btn btn-info mr-5'>Earnings</NavLink>
+      </nav>
+    );
+  }
+
+}
+
+export default Nav;
