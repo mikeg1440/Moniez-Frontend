faec4f5bc926b6fb400683b1ccd1aa718cb9aab1
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-15 12:48:26 -0500

    Add redux, reducers, store, and thunk configuration to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 import 'bootstrap/dist/css/bootstrap.css';
 import './CSS/App.css';
 import App from './components/App';
 import AppLayout from './layouts/AppLayout';
 
+const composeEnhancer = compose(
+  applyMiddleware(thunk),
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+const store = createStore(rootReducer, composeEnhancer)
+
 ReactDOM.render(
-  <AppLayout>
-    <App />
-  </AppLayout>
+  <Provider store={store}>
+    <AppLayout>
+        <App />
+    </AppLayout>
+</Provider>
   , document.getElementById('root'));
