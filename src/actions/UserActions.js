88c1555c1edebd1471726beb3fcffa0e385f30f9
Mike Gaudreau <gaudreaum01@gmail.com>
2020-02-15 14:52:30 -0500

    Add fetch request to register user in UserActions

--- src/actions/UserActions.js
+++ src/actions/UserActions.js
@@ -1,4 +1,17 @@
 
-export const registerUser = (dispatch) => {
-  
+export function registerUser(userInfo) {
+  return (dispatch) => {fetch('http://localhost:3000/api/v1/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({user: userInfo})
+  }).then(resp => {
+    console.log(resp.headers)
+    return resp.json()
+  })
+  .then(console.log)
+  .catch(err => console.log(`Error Caught: ${err}`))
+
+  }
 }
