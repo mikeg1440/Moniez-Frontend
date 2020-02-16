43927189a803a26a3f305fecd086ce63c65c8f30
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-16 16:35:38 -0500

    Add actions for user registeration and login in UserActions.js

--- src/actions/UserActions.js
+++ src/actions/UserActions.js
@@ -1,17 +1,47 @@
 
-export function registerUser(userInfo) {
-  return (dispatch) => {fetch('http://localhost:3000/api/v1/register', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({user: userInfo})
-  }).then(resp => {
-    console.log(resp.headers)
-    return resp.json()
-  })
-  .then(console.log)
-  .catch(err => console.log(`Error Caught: ${err}`))
+export function registerUser(userInfo, props) {
+  return (dispatch) => {
+    fetch('http://localhost:3000/api/v1/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({user: userInfo})
+    })
+    .then(resp => resp.json())
+    .then(data => {
+      console.log(data)
+      if (Object.keys(data).includes('id')){
+        dispatch({type: 'LOGIN_USER', payload: data})
+        props.history.push('/dashboard')
+      }else {
+        dispatch({type: 'USER_ERROR', payload: data})
+      }
+    })
+    .catch(err => console.log(`Error Caught: ${err}`))
+  }
+}
 
+export function loginUser(userInfo, props){
+
+  return (dispatch) => {
+    fetch('http://localhost:3000/api/v1/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({user: userInfo})
+    })
+    .then(resp => resp.json())
+    .then(data => {
+      console.log(data)
+      if (Object.keys(data).includes('id')){
+        dispatch({type: 'LOGIN_USER', payload: data})
+        props.history.push('/dashboard')
+      }else {
+        dispatch({type: 'USER_ERROR', payload: data})
+      }
+    })
+    .catch(console.log)
   }
 }
