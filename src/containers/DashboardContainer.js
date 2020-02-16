84dccf34c94b39f5129634f5c1af378dc04cf290
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-16 17:10:10 -0500

    Add import and implement new DashboardNav component to DashboardContainer

--- src/containers/DashboardContainer.js
+++ src/containers/DashboardContainer.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import DashboardNav from '../components/DashboardNav';
 
 class DashboardContainer extends Component {
+
 
   render() {
     return (
       <div className='text-center'>
-        <h2>Dashboard Conatiner</h2>
+        <h2>Dashboard Container</h2>
+
+        <div className='row mr-auto ml-auto'>
+          <DashboardNav />
+        </div>
+
         <div className='row'>
           <div className='col'>
             Budget Entries
