fc4a4cff4b2b8fc4889768fe963ad22127b3d801
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-16 16:39:15 -0500

    Add a higher order component called PublicRoute to redirect authenticated users away (Not implemented yet)

---
+++ src/components/PublicRoute.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Route, Redirect} from 'react-router-dom';
+
+const PublicRoute = ({component: Component, ...rest}) => (
+  <Route {...rest} render={props => ( 
+    localStorage.getItem('token') ? (
+      <Redirect to={{
+        pathname: '/dashboard',
+        state: { from: props.location}
+      }} />
+    ) : (
+      <Component {...props} />
+    )
+  )}/>
+
+);
+
+export default PublicRoute;
