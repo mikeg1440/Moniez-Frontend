baa586086d4abcf6f98e1d57090e2561195e7043
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-16 17:09:44 -0500

    Add a DashboardNav component to display different budget elements

---
+++ src/components/DashboardNav.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import {NavLink} from 'react-router-dom';
+
+
+class DashboardNav extends Component {
+
+  handleLogout = () => {
+    alert('Loggin user out!')
+  }
+
+
+  render() {
+    return (
+      <div className='col'>
+        <NavLink to='/earnings' className='btn btn-info mr-5' >Earnings</NavLink>
+        <NavLink to='/expenses' className='btn btn-info mr-5' >Expenses</NavLink>
+        <NavLink to='/bills' className='btn btn-info' >Bills</NavLink>
+        <button className='btn btn-warning float-right' onClick={this.handleLogout}>Logout</button>
+      </div>
+    );
+  }
+
+}
+
+export default DashboardNav;
