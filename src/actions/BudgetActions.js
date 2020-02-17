cb28f86110611b751e285cba29f539e67a772b1e
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-17 10:11:31 -0500

    Add addBudget and getBudgets actions to the BudgetActions file

--- src/actions/BudgetActions.js
+++ src/actions/BudgetActions.js
@@ -1,19 +1,43 @@
 
 const authToken = () => (localStorage.getItem('token'))
 
+const requestHeaders = () => {
+  let headers = new Headers()
+  headers.append("Content-Type", "application/json")
+  headers.append("bearer", authToken())
+  return headers
+}
+
+const getOptions = () => ({
+  method: 'GET',
+  headers: requestHeaders(),
+  redirect: 'follow'
+})
+
+const postOptions = (infoObject) => ({
+  method: 'POST',
+  headers: requestHeaders(),
+  body: JSON.stringify(infoObject),
+})
+
 export function getBudgets(){
-  // const token = localStorage.getItem('token')
+  debugger
+  return (dispatch) => {
+    fetch("http://localhost:3000/api/v1/budgets", getOptions())
+    .then(response => response.json())
+    .then(data => dispatch({type: 'SET_BUDGETS', payload: data}))
+    .catch(console.log);
+  }
+
+}
+
+export function addBudget(budgetInfo){
 
   return (dispatch) => {
-    fetch('http://localhost:3000/api/v1/budgets', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'bearer': authToken,
-      }
-    })
-    .then(resp => resp.json())
-    .then(console.log)
+    fetch("http://localhost:3000/api/v1/budgets", postOptions({budget: budgetInfo}))
+    .then(response => response.json())
+    .then(data => dispatch({type: 'ADD_BUDGET', payload: data}))
     .catch(console.log)
   }
+
 }
