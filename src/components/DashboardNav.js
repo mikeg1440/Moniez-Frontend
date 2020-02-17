4e4fc06f2cc65ef7f6d88a339532833bcf0adb56
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-17 15:02:14 -0500

    Refactor DashboardNav so it only includes the links for earnings, bills, and expense so we can make it a child of MainNav

--- src/components/DashboardNav.js
+++ src/components/DashboardNav.js
@@ -4,11 +4,6 @@ import {NavLink} from 'react-router-dom';
 
 class DashboardNav extends Component {
 
-  // handleLogout = () => {
-  //   alert('Loggin user out!')
-  // }
-
-
   render() {
     return (
       <>
@@ -22,13 +17,3 @@ class DashboardNav extends Component {
 }
 
 export default DashboardNav;
-
-// return (
-//   <div className='row'>
-//     <div className='col'>
-//       <NavLink to='/earnings' className='btn btn-info mr-5' >Earnings</NavLink>
-//       <NavLink to='/expenses' className='btn btn-info mr-5' >Expenses</NavLink>
-//       <NavLink to='/bills' className='btn btn-info' >Bills</NavLink>
-//     </div>
-//   </div>
-// );
