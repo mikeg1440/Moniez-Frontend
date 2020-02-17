c502015224cd1f08243139fce4966ad0c7b35126
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-17 10:19:30 -0500

    Remove unused imports from LoginOptions

--- src/components/LoginOptions.js
+++ src/components/LoginOptions.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom'
 import dollarLogo from '../images/dollar-logo.png';
-// import SignUp from '../components/forms/SignUp';
 
 const LoginOptions = () => (
   <div className='container text-center p-5'>
