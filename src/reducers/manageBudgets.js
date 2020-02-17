f0b45a6fc1b9d88e757ac74e6d8ba6a657028c88
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-17 10:03:12 -0500

    Add logic to manageBudgets to set budgets from a fetch request to the backend api

--- src/reducers/manageBudgets.js
+++ src/reducers/manageBudgets.js
@@ -1,9 +1,12 @@
 
 export default function manageBudgets(state = [], action) {
   switch(action.type){
-    case 'SET_BUDGET':
+    case 'SET_BUDGETS':
       debugger
-      return {...action.payload}
+      return action.payload
+    case 'ADD_BUDGET':
+      debugger
+      return state.concat(action.payload)
     default:
       return state
   }
