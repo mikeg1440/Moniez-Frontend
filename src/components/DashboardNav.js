bcb1b49d854ea97bee814b90ecf2841fa5682b30
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-17 14:58:38 -0500

    Refactor DashboardNav so we can call it as a child of MainNav

--- src/components/DashboardNav.js
+++ src/components/DashboardNav.js
@@ -4,23 +4,31 @@ import {NavLink} from 'react-router-dom';
 
 class DashboardNav extends Component {
 
-  handleLogout = () => {
-    alert('Loggin user out!')
-  }
+  // handleLogout = () => {
+  //   alert('Loggin user out!')
+  // }
 
 
   render() {
     return (
-      <div className='col'>
-        <NavLink to='/budgets' className='btn btn-success mr-5 float-left' >Budgets</NavLink>
-        <NavLink to='/earnings' className='btn btn-info mr-5' >Earnings</NavLink>
-        <NavLink to='/expenses' className='btn btn-info mr-5' >Expenses</NavLink>
-        <NavLink to='/bills' className='btn btn-info' >Bills</NavLink>
-        <button className='btn btn-warning float-right' onClick={this.handleLogout}>Logout</button>
-      </div>
+      <>
+      <NavLink to='/earnings' className='btn btn-info mr-5' >Earnings</NavLink>
+      <NavLink to='/expenses' className='btn btn-info mr-5' >Expenses</NavLink>
+      <NavLink to='/bills' className='btn btn-info' >Bills</NavLink>
+      </>
     );
   }
 
 }
 
 export default DashboardNav;
+
+// return (
+//   <div className='row'>
+//     <div className='col'>
+//       <NavLink to='/earnings' className='btn btn-info mr-5' >Earnings</NavLink>
+//       <NavLink to='/expenses' className='btn btn-info mr-5' >Expenses</NavLink>
+//       <NavLink to='/bills' className='btn btn-info' >Bills</NavLink>
+//     </div>
+//   </div>
+// );
