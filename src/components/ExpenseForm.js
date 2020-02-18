bf18ef65779086cfa31d8214a007473e2024b462
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-18 16:49:12 -0500

    Create a expense form component to handle prompt user for input on creating a new expense

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+
+const initialState = {
+  expense_category_id: 1,
+  amount: 0
+}
+
+class ExpenseForm extends Component {
+  constructor(props){
+    super(props)
+    this.state = initialState
+
+  }
+
+  handleChange = (e) => {
+    const {name, value} = e.target
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.callback(this.state)
+    this.setState(initialState)
+  }
+
+  render() {
+    return (
+      <div className='container p-5' id='budgetForm'>
+        <form onSubmit={this.handleSubmit}>
+
+          <div className='row mb-2 '>
+            <div className='col'>
+              <label className='float-right'>Expense Category: </label>
+            </div>
+
+            <div className='col-9'>
+              <select name='expense_category_id' onChange={this.handleChange}>
+                {this.props.categories.map(category => <option key={category.id} value={category.id}>{category.title}</option>)}
+              </select>
+
+            </div>
+          </div>
+
+          <div className='row'>
+            <div className='col-3'>
+              <label className='float-right'>Expense Amount: </label>
+            </div>
+
+            <div className='col-9'>
+              <input
+                name='amount'
+                className='form-control'
+                value={this.state.amount}
+                onChange={this.handleChange}
+                placeholder='Amount' />
+              </div>
+          </div>
+
+
+          <div className='row mt-2'>
+            <div className='col text-center'>
+              <input type='submit' value='Create Expense' className='btn btn-primary' />
+            </div>
+          </div>
+
+
+        </form>
+      </div>
+    )
+  }
+}
+
+export default ExpenseForm;
