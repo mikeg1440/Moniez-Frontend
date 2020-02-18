b0d560758ca62e3118b270883501fa39adfc126e
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-18 09:50:45 -0500

    Add new category manager to the reducer index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import currentUser from './manageUsers';
 import budgets from './manageBudgets';
+import categories from './manageCategories';
 
 export default combineReducers({
   currentUser,
-  budgets
+  budgets,
+  categories
 });
