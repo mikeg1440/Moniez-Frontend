e4775add8150509ca1a060e4c37c0f40cdaad838
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-18 16:42:02 -0500

    Add GET_CATEGORIES case to manageCategories

--- src/reducers/manageCategories.js
+++ src/reducers/manageCategories.js
@@ -6,6 +6,8 @@ export default function manageCategories(state = {
   switch(action.type){
     case 'GET_EARNING_CATEGORIES':
       return {...state, earnings: action.payload}
+    case 'GET_CATEGORIES':
+      return action.payload
     default:
       return state
   }
