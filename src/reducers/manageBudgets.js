d5bef0990e7e83a730d08dd77cbe2c7a78d22002
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-18 09:50:21 -0500

    Refactor manageBudgets to include more defaults and a ADD_EARNING case to the switch statement

--- src/reducers/manageBudgets.js
+++ src/reducers/manageBudgets.js
@@ -1,19 +1,24 @@
 
 export default function manageBudgets(state = {
   all: [],
-  selected: {}
+  selected: {},
+  earnings: [],
+  bills: [],
+  expenses: []
 }, action) {
   switch(action.type){
     case 'SET_BUDGETS':
       return {...state, all: action.payload}
     case 'ADD_BUDGET':
-      debugger
       return {...state, all: state.concat(action.payload)}
     case 'SELECT_BUDGET':
       let selected = state.all.filter(budget => budget.id === parseInt(action.payload))[0]
+      localStorage.setItem('current_budget_id', selected.id)
       return {...state, selected: selected}
     case 'SET_EARNINGS':
-      return {...state, selected.earnings = action.payload}
+      return {...state, earnings: action.payload}
+    case 'ADD_EARNING':
+      return {...state, earnings:[ ...state.earnings, action.payload]}
     default:
       return state
   }
