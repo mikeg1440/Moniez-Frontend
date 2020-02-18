0974730113ae650eac7ab0e3f3f958a07a9b7e6b
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-18 09:52:55 -0500

    Add a getEarningCategories and addEarning action to BudgetActions

--- src/actions/BudgetActions.js
+++ src/actions/BudgetActions.js
@@ -47,7 +47,27 @@ export function getEarnings(){
   return (dispatch) => {
     fetch("http://localhost:3000/api/v1/earnings", getOptions())
     .then(response => response.json())
-    .then(data => dispatch({type: 'SET_BUDGETS', payload: data}))
+    .then(data => dispatch({type: 'SET_EARNINGS', payload: data}))
     .catch(console.log);
+  }
+}
+
+export function getEarningCategories(){
+  return (dispatch) => {
+    fetch("http://localhost:3000/api/v1/categories/earning_categories", getOptions())
+    .then(response => response.json())
+    .then(data => dispatch({type: 'GET_EARNING_CATEGORIES', payload: data}))
+    .catch(console.log);
+  }
+}
+
+
+export function addEarning(earningInfo){
+  debugger
+  return (dispatch) => {
+    fetch("http://localhost:3000/api/v1/earnings", postOptions({earning: earningInfo}))
+    .then(response => response.json())
+    .then(data => dispatch({type: 'ADD_EARNING', payload: data}))
+    .catch(console.log)
   }
 }
