1b182329d1dc1e6babef9744960d9a25a26adff8
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-19 17:08:03 -0500

    Add logout feature to MainNav

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,22 +1,40 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Redirect } from 'react-router-dom';
+import {endUserSession} from '../actions/UserActions';
+import {connect} from 'react-redux';
 
 class MainNav extends Component {
+  state = {
+    navigate: false
+  }
 
   handleLogout = () => {
-    alert('Loggin user out!')
+    let logout = window.confirm('You are about to be logged out!')
+    logout && this.props.endUserSession()
+    localStorage.removeItem('token')
+    localStorage.removeItem('current_budget_id')
+    this.setState({
+      navigate: true
+    })
   }
 
   renderBackButton = () => (
     <NavLink to='/dashboard' className='btn btn-info'>Dashboard</NavLink>
   )
 
+  renderBudgetButton = () => (
+    <NavLink to='/budgets' className='btn btn-success mr-5 float-left' >Budgets</NavLink>
+  )
+
   render() {
+    if (this.state.navigate){
+      return <Redirect to='/' push={true} />
+    }
     return (
       <nav className='text-center'>
         <div className='row'>
           <div className='col'>
-            <NavLink to='/budgets' className='btn btn-success mr-5 float-left' >Budgets</NavLink>
+            {this.props.location.pathname !== '/budgets' && this.renderBudgetButton()}
             {this.props.children}
             {this.props.location.pathname !== '/dashboard' ? this.renderBackButton() : null}
             <button className='btn btn-warning float-right' onClick={this.handleLogout}>Logout</button>
@@ -28,4 +46,4 @@ class MainNav extends Component {
 
 }
 
-export default MainNav;
+export default connect(null, {endUserSession})(MainNav);
