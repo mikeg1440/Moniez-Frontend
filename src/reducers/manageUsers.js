f28eeaa4bdec2723b11097332b2d2d74c8167fbc
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-19 16:51:30 -0500

    Remove logout_user case statement from manageUser

--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -7,9 +7,6 @@ export default function manageUsers(state = {
       const {id, username, email, authentication_token} = action.payload
       localStorage.setItem('token', authentication_token)
       return {...state, isLoggedIn: true, id, username, email}
-    case 'LOGOUT_USER':
-      localStorage.removeItem('token')
-      return {isLoggedIn: false}
     case 'USER_ERROR':
       return {...state, errors: [action.payload.errors]}
     case 'CLEAR_ERRORS':
