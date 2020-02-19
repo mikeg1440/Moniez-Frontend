498704bb0f5ce713c11a969d2dff57e21ff19dc3
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-19 16:58:20 -0500

    Add new logo to LoginOptions

--- src/components/LoginOptions.js
+++ src/components/LoginOptions.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-import dollarLogo from '../images/dollar-logo.png';
+import appLogo from '../images/moniez-green-logo.png';
 
 const LoginOptions = () => (
   <div className='container text-center p-5'>
 
     <div className='text-center pb-5'>
-      <img src={dollarLogo} style={{width: '10rem'}} alt='Dollar Sign Logo' />
+      <img src={appLogo} style={{width: '15rem'}} alt='Dollar Sign Logo' />
     </div>
 
     <Link to='/sign-up' className='btn btn-primary mr-5'>Sign Up</Link>
 
     <Link to='/login' className='btn btn-primary mr-5'>Login</Link>
 
-    <Link to='/dashboard' className='btn btn-primary'>Dashboard</Link>
+    {/* <Link to='/dashboard' className='btn btn-primary'>Dashboard</Link> */}
   </div>
 );
 
