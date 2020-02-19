bdb0123e389b0580abaa2c0a66624fccd4e400ac
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-19 16:52:17 -0500

    Add END_SESSION inside index.js file within reducer folder

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,8 +3,25 @@ import currentUser from './manageUsers';
 import budgets from './manageBudgets';
 import categories from './manageCategories';
 
-export default combineReducers({
+// export default combineReducers({
+//   currentUser,
+//   budgets,
+//   categories
+// });
+
+const appReducer = combineReducers({
   currentUser,
   budgets,
   categories
 });
+
+
+const rootReducer = (state, action) => {
+  if (action.type === 'END_SESSION'){
+    state = undefined
+  }
+
+  return appReducer(state, action)
+}
+
+export default rootReducer
