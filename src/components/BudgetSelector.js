2aff0b0c70fc52041fbcf403182106e0d842f772
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-19 17:16:00 -0500

    Fix budget selector by making a class component with state so it keeps track of the currently selected budget

--- src/components/BudgetSelector.js
+++ src/components/BudgetSelector.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const BudgetSelector = ({budgets, callback}) => {
+const currentBudgetId = () => (parseInt(localStorage.getItem('current_budget_id')))
 
+class BudgetSelector extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      budgetId: currentBudgetId()
+    }
+  }
 
-  return (
-    <div>
-      <select onChange={callback} className="mdb-select md-form">
-        <option value="" disabled selected hidden>Select a Budget</option>
+  handleChange = (e) => {
+    const budgetId = e.target.options[e.target.selectedIndex].value
+    this.setState({
+      budgetId: budgetId
+    })
+    this.props.callback(budgetId)
+
+  }
+
+  render() {
+    const {budgets} = this.props
+    if (budgets.all.length === 0){
+      return (
+        <h2>You must first create a budget by clicking the Budgets button!</h2>
+      )
+    }
+    return (
+      <select onChange={this.handleChange} className="mdb-select md-form budget-selector" value={this.state.budgetId}>
+        {!currentBudgetId && <option value="" disabled selected hidden>Select a Budget</option> }
         {budgets.all.map(budget => <option key={budget.id} value={budget.id}>{budget.title}</option>)}
       </select>
-    </div>
-  )
+    );
+  }
+
 }
 
 export default BudgetSelector;
