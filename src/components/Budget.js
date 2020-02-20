bb263e01ae9d78bf27ddc06404e46fa70e052769
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-20 14:00:13 -0500

    Fix elements in Budget component that have class set to be className

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Budget = ({budget, deleteAction}) => (
   <div className='col'>
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">{budget.title}</h5>
-        <p class="card-text">{budget.description}</p>
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">{budget.title}</h5>
+        <p className="card-text">{budget.description}</p>
         <div className='text-center'>
           <h1>${budget.total}</h1>
         </div>
