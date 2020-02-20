64f37e82d3db65385f8f69cc70e749e5e8375121
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-20 11:43:12 -0500

    Remove the unneeded Bill, Expense and Earning components that are no longer used

--- src/components/Bill.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Bill = ({bill, deleteAction}) => (
-  <>
-      <li>{bill.bill_category.title} - ${bill.amount} <button className='ml-3 btn btn-danger' onClick={() => deleteAction(bill.id)}>X</button></li>
-  </>
-);
-
-export default Bill;
