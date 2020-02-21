d6bcbe7b8e03b8d42ed92803b7c24defe3360465
Michael Gaudreau <gaudreaum01@gmail.com>
2020-02-20 20:07:24 -0500

    Add a conditional statement that redirects user to the dashboard if they are already logged in by checking for a token in the localStorage cache

--- src/components/LoginOptions.js
+++ src/components/LoginOptions.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
 import appLogo from '../images/moniez-green-logo.png';
 
-const LoginOptions = () => (
-  <div className='container text-center p-5'>
+const LoginOptions = () =>{
 
-    <div className='text-center pb-5'>
-      <img src={appLogo} style={{width: '15rem'}} alt='Dollar Sign Logo' />
+
+  return localStorage.getItem('token') ? (
+    <Redirect to='/dashboard' />
+  ) : (
+    <div className='container text-center p-5'>
+
+      <div className='text-center pb-5'>
+        <img src={appLogo} style={{width: '15rem'}} alt='Dollar Sign Logo' />
+      </div>
+
+      <Link to='/sign-up' className='btn btn-primary mr-5'>Sign Up</Link>
+
+      <Link to='/login' className='btn btn-primary'>Login</Link>
+
     </div>
-
-    <Link to='/sign-up' className='btn btn-primary mr-5'>Sign Up</Link>
-
-    <Link to='/login' className='btn btn-primary'>Login</Link>
-
-  </div>
-);
-
+  );
+}
 export default LoginOptions;
