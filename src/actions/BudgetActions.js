9935d37c72f2c76e20ff169c6ffc45083dc2fdb6
Michael Gaudreau <gaudreaum01@gmail.com>
2020-03-02 19:24:41 -0500

    Remove unneeded functions that are no longer used in this file imported from requestHelpers

--- src/actions/BudgetActions.js
+++ src/actions/BudgetActions.js
@@ -1,5 +1,5 @@
 
-import {requestHeaders, getOptions, postOptions, deleteOptions, authToken} from '../helpers/requestHelpers'
+import { getOptions, postOptions, deleteOptions } from '../helpers/requestHelpers'
 
 
 export function getBudgets(){
