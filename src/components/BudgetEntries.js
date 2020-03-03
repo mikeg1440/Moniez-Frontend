00301338d0ca9256b2bcc6ccaa827e34ea4b299a
Michael Gaudreau <gaudreaum01@gmail.com>
2020-03-02 19:56:03 -0500

    Add conditional class to BudgetEntries component to show text in green if total is >= 0 or else show in red

--- src/components/BudgetEntries.js
+++ src/components/BudgetEntries.js
@@ -5,7 +5,7 @@ class BudgetEntries extends PureComponent {
   render() {
     const {budget} = this.props
     return (
-      <tr>
+      <tr className={ budget.total >= 0 ? 'inGreen' : 'inRed'}>
         <td> {budget.title} </td>
   			<td> $ {budget.total}</td>
       </tr>
