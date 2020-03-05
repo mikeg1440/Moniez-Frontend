10aaf467b8ab4670968565746bbc43dbd0c5fa7f
Michael Gaudreau <gaudreaum01@gmail.com>
2020-03-04 21:06:15 -0500

    Fix error being thrown when we only have a single error to display with AlertMessage component

--- src/components/AlertMessage.js
+++ src/components/AlertMessage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AlertMessage = ({errors}) =>{
   return (
   <div className='alert alert-danger'>
-    {errors[0].map((error, index) => <h4 key={index}>{error}</h4>)}
+    {errors.length > 1 ? errors[0].map((error, index) => <h4 key={index}>{error}</h4>) : <h4 key='error'>{errors[0]}</h4> }
   </div>
 );}
 
