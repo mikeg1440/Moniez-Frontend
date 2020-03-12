ce4dc9c2316bcf195c8db8742b5e3a11e04c9262
Michael Gaudrau <gaudreaum01@gmail.com>
2020-03-12 16:23:40 -0400

    Fix AlertMessage component to work with the fix we implemented

--- src/components/AlertMessage.js
+++ src/components/AlertMessage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AlertMessage = ({errors}) =>{
   return (
   <div className='alert alert-danger'>
-    {errors.length > 1 ? errors[0].map((error, index) => <h4 key={index}>{error}</h4>) : <h4 key='error'>{errors[0]}</h4> }
+    {errors.length > 1 ? errors.map((error, index) => <h4 key={index}>{error}!</h4>) : <h4 key='error'>{errors[0]}!</h4> }
   </div>
 );}
 
